b513d8ba7301b41c225107f54deb6df6167b4ceb
dt1219 <276218102@qq.com>
2016-03-02 10:27:16 +0800

    week 2 task

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-#sth
+        if (nrow(x)!=ncol(x)) { 
+                message("Error in solve.default(x), matrix must be square")
+        }
+        else {
+                sol <- NULL
+                set <- function(y) {
+                        x <<- y
+                        sol <<- NULL
+                }
+                get <- function() x
+                setsol <- function(solve) sol <<- solve
+                getsol <- function() sol
+                list(set = set, get = get,
+                     setsol = setsol,
+                     getsol = getsol)
+        }
 }
 
 
@@ -12,4 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        sol <- x$getsol()
+        if(!is.null(sol)) {
+                message("getting cached data")
+                return(sol)
+        }
+        data <- x$get()
+        sol <- solve(data, ...)
+        x$setsol(sol)
+        sol
 }
